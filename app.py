f493891809dcfb2e8d1f65a0ca3a70840c762d5e
SIMPLON <tellai>
2021-03-23 14:34:23 +0100

    logger

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+import logging
+from logging import FileHandler
 from flask import Flask, request, render_template, redirect, url_for
 from mail import send_mail
 app = Flask(__name__)
+
+logging.basicConfig(filename = "web_scrapping.log", level= logging.DEBUG, 
+                    format='%(asctime)s - %(name)s -%(levelname)s - %(message)s')
+
 
 @app.route('/')
 def my_form():
@@ -8,10 +14,13 @@ def my_form():
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-    if request.method == 'POST':
-        email = request.form['text']
-        send_mail(email)
-    return redirect(url_for("my_form"))
+    try:
+        if request.method == 'POST':
+            email = request.form['text']
+            send_mail(email)
+        return redirect(url_for("my_form"))
+    except Exception as e:
+        logging.error("split method in carpet descripsion is expecting a bytes-like object" +str(e))
 
 
 if __name__ == '__main__':
